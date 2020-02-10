71102df5efeb42ffc77da588fd3a70f1de4b0c8b
MichaelZaleskovsky <mz.java.developer@gmail.com>
2020-02-10 12:54:47 +0200

    Project structure

---
+++ BackEnd/src/main/java/com/iAmJunior/IAmJunior/IAmJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.iAmJunior.IAmJunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAmJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAmJuniorApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/iAmJunior/IAmJunior/IAmJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iAmJunior.IAmJunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAmJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
